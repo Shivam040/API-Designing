2fa1daea3097758c6adaac9a8862bb40ad030302
Shivam Dangwal <shivamdangwal04@gmail.com>
2026-08-29 14:51:58 +0530

    Add pydantic file for Example 7

---
+++ Basic/1 HTTP and API fundamentals/Example 7/pydantic_model.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, EmailStr, Field
+
+
+class UserCreate(BaseModel):
+    name: str = Field(min_length=2, max_length=100)
+    email: EmailStr
+
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
+
